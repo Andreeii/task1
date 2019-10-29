7e00d9a7b0bd4a4688b3458f7a45fa0a77c3f7e6
Țurcanu Andrei <andrei.turcanu@unifun.com>
2019-10-29 12:09:27 +0200

    show two numbers from an array wich sum is aqual to given constant

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+
+
+
+        int a = 9 ;
+        int  arr[] = {2,7,11,4,5};
+
+        for (int i = 0 ; i < arr.length ; i++){
+
+            for(int j = 0 ; j < arr.length ; j ++){
+                if( arr[i]+ arr[j] == a ){
+                    System.out.print(i);
+                    System.out.print(" ");
+                    System.out.print(j);
+                    System.out.print(" | ");
+                }
+            }
+        }
+    }
+}
